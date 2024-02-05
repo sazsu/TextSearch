7c0ae2a34b5e7bc5716103690d4d56328a6ebcf7
gorm1335 <shonniy@yandex.ru>
2024-02-05 21:41:00 +0300

    регистр букв

--- main.py
+++ main.py
@@ -5,11 +5,19 @@ def ispresent(path_to_file: str) -> bool:
     return os.path.isfile(path_to_file)
 
 
-def count_appearance(path_to_file: str, word=None, *args, **kwargs) -> str:
+def count_appearance(path_to_file: str, word=None, letter_case=None, *args, **kwargs) -> str:
     if not ispresent(path_to_file):
         return 'такого файла не существует'
     
     with open(path_to_file, 'r', encoding='utf-8') as f:
-        appearance_cnt = f.read().count(word)
+        match letter_case:  # проверяем доп аргумент для регистра
+            case None:
+                appearance_cnt = f.read().count(word)
+            case '-u':  # верхний регистр
+                appearance_cnt = f.read().upper().count(word)
+            case '-l':  # нижний регистр
+                appearance_cnt = f.read().lower().count(word)
+            case _:
+                appearance_cnt = 'неверный аргумент letter_case'
     
-    return appearance_cnt if appearance_cnt else 'не встретилось'
+    return appearance_cnt if appearance_cnt != 0 else 'не встретилось'
