7e752ba976cc6f32aeda8c20f3bc17ab1a2996b1
gorm1335 <shonniy@yandex.ru>
2024-02-05 20:48:12 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import os.path
+
+
+def ispresent(path_to_file: str) -> bool:
+    return os.path.isfile(path_to_file)
+
+
+def count_appearance(path_to_file: str, word=None, *args, **kwargs) -> str:
+    if not ispresent(path_to_file):
+        return 'такого файла не существует'
+    
+    with open(path_to_file, 'r', encoding='utf-8') as f:
+        appearance_cnt = f.read().count(word)
+    
+    return appearance_cnt if appearance_cnt else 'не встретилось'
+
+
+print(count_appearance('testfile.txt', 'hello'))
